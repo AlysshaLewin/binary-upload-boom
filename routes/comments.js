aa8dfd5fc1385def4a666cfc16d1d45c6a5005d4
Alyssha <90856589+AlysshaLewin@users.noreply.github.com>
2022-10-03 21:18:14 -0500

    removed comments section

--- routes/comments.js
+++ routes/comments.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const commentsController = require("../controllers/comments");
+//const commentsController = require("../controllers/comments");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Comment Routes - simplified for now
-router.post("/createComment/:id", commentsController.createComment);
+//router.post("/createComment/:id", commentsController.createComment);
 
 
 module.exports = router;
