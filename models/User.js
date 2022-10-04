42c126a00f0adc4d9cacccc2219af82b8c1260aa
Alyssha <90856589+AlysshaLewin@users.noreply.github.com>
2022-10-03 21:57:31 -0500

    added password hash

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   userName: { type: String, unique: true },
   email: { type: String, unique: true },
-  password: String,
+  password: { type: String, required: true },
+  date: { type: Date, default: Date.now }
 });
 
 // Password hash middleware.
