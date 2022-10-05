74d1d43d9f883ca56d4820d8d6f896b74a103c47
Alyssha <90856589+AlysshaLewin@users.noreply.github.com>
2022-10-04 21:21:16 -0500

    commented out comments controller

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,30 +1,30 @@
-const Comment = require("../models/Comment");
+//const Comment = require("../models/Comment");
 
 //add Post model
-const Post = require("../models/Post");
+//const Post = require("../models/Post");
 
-module.exports = {
-  createComment: async (req, res) => {
-    try {
-      // creates a new comment
-      await Comment.create({
-        comment: req.body.comment,
-        likes: 0,
-        post: req.params.id,
-      });
+// module.exports = {
+//   createComment: async (req, res) => {
+//     try {
+//       // creates a new comment
+//       await Comment.create({
+//         comment: req.body.comment,
+//         likes: 0,
+//         post: req.params.id,
+//       });
 
-      console.log("Comment has been added!");
-      res.redirect("/post/"+req.params.id);
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  deleteComments: async (req, res) => {
-    try {
-      await Comment.deleteOne({_id: req.params.commentid})
-      res.redirect("/post/"+req.parmas.postid);
-    } catch(err) {
-      console.log(err);
-    }
-  }
-};
+//       console.log("Comment has been added!");
+//       res.redirect("/post/"+req.params.id);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   },
+//   deleteComments: async (req, res) => {
+//     try {
+//       await Comment.deleteOne({_id: req.params.commentid})
+//       res.redirect("/post/"+req.parmas.postid);
+//     } catch(err) {
+//       console.log(err);
+//     }
+//   }
+// };
