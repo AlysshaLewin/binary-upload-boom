bf602ffbeae5a11185bec9fac7797a432b83b518
Alyssha Lewin <alyssha.lew@gmail.com>
2022-10-05 14:54:06 -0500

    fixed request routes

--- public/js/index.js
+++ public/js/index.js
@@ -17,7 +17,7 @@ $("#update_user").submit(function(event){
 
 
     var request = {
-        "url" : `http://localhost:8800/api/users/${data.id}`,
+        "url" : `http://localhost:8500/api/users/${data.id}`,
         "method" : "PUT",
         "data" : data
     }
@@ -34,7 +34,7 @@ if(window.location.pathname == "/profile"){
         var id = $(this).attr("data-id")
 
         var request = {
-            "url" : `http://localhost:8800/api/users/${id}`,
+            "url" : `http://localhost:8500/api/users/${id}`,
             "method" : "DELETE"
         }
 
