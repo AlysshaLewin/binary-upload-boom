82978535531c337ed76c847a807e6a779e1696db
Alyssha <90856589+AlysshaLewin@users.noreply.github.com>
2022-10-09 13:29:43 -0500

    required form controller and added API

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const postsController = require("../controllers/posts");
+const formController = require("../controllers/form");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
@@ -15,5 +16,15 @@ router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
 router.get("/add-user", ensureAuth, postsController.addUser);
 router.get("/update-user", ensureAuth, postsController.updateUser);
+
+
+
+
+// API
+router.post('/api/users', formController.create);
+router.get('/api/users', formController.find);
+router.put('/api/users/:id', formController.update);
+router.delete('/api/users/:id', formController.delete);
+
 
 module.exports = router;
